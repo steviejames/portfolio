e304314207e03074a6a718ff5dc5b7b41aba7c72
steviejames <55714306+steviejames@users.noreply.github.com>
2020-09-09 07:10:55 +0100

    Add files via upload

---
+++ precache-manifest.f6bce750fb37833a7b6a97a349bee805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94819aadb36f64a44ad81f98ccd27b02",
+    "url": "/steviejames/portfolioindex.html"
+  },
+  {
+    "revision": "530ff7a4f0e0cf2e138d",
+    "url": "/steviejames/portfoliostatic/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "53b422601703ff8912c4",
+    "url": "/steviejames/portfoliostatic/js/2.9dc783dc.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/steviejames/portfoliostatic/js/2.9dc783dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530ff7a4f0e0cf2e138d",
+    "url": "/steviejames/portfoliostatic/js/main.96ee1d6c.chunk.js"
+  },
+  {
+    "revision": "07e76554517686ed407b",
+    "url": "/steviejames/portfoliostatic/js/runtime-main.abc91191.js"
+  }
+]);
